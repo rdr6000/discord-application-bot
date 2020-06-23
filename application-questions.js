bd107fe450922ad9729ea4ae953013dd980063c0
rdr6000 <40518544+rdr6000@users.noreply.github.com>
2020-06-23 15:49:37 +0530

    Update application-questions.js

--- application-questions.js
+++ application-questions.js
@@ -1,5 +1,5 @@
 module.exports =  [
-	"question 1",
-	"question 2",
-	"question 3"
+"What is you are Age? You need to be minimum 16 years old to play competitive.", "What is your In Game Name (ign)?", "What is your Character Id?", "What is your kill death ratio (k.d) out of how many matches?", "What is your current season tier?","How much time will you play PUBG in a day?" "Do you know Bengali? If not, what language do you communicate in.", "What role do u have in a game play (Sniper,Flanker, Assaulter etc)? Which device do you play on?", "Do u have any competitive experience? Do u have rename card?", "What are you applying for?(Competitive/Casual/Content Creator)?","Enter your whatsapp number for communication. (Not compulsory)"
+
+
 ];
